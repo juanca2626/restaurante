406ae5fa10d0380e29d2e6c9d5bb0142f34b3a16
Diter Huanhuayo <diterh@gmail.com>
2024-12-25 10:12:27 -0500

    ajustando catalogo

---
+++ lang/vendor/cookie-consent/es/texts.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'message' => 'Utilizamos cookies para mejorar tu experiencia en el sitio y optimizar nuestros esfuerzos de marketing. Al continuar navegando, aceptas nuestra <a href="https://www.appkyte.com/politica-de-privacidad?utm_medium=product-sharing&amp;utm_source=catalog&amp;utm_campaign=cookies&amp;_gl=1*1ytmvl1*_gcl_au*MTMyMjM3OTUyMy4xNzM0MDM1MzIx*_ga*MTk4OTg4ODA1MS4xNzM0MDM1MzIx*_ga_41YGQTYJJS*MTczNTA3NjM4NS41LjAuMTczNTA3NjM4NS42MC4wLjI1MDc1MTkzNg.." target="_blank" rel="noreferrer" class="text-md font-medium sm:text-sm text-theme-2-dark">política de privacidad</a>',
+    'agree' => 'Aceptar y cerrar',
+];
