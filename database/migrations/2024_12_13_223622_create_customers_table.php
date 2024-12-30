ce39c0baaee3898d5e94de8fa76e145f8f190e71
Diter Huanhuayo <diterh@gmail.com>
2024-12-30 17:35:54 -0500

    cambios customer.

--- database/migrations/2024_12_13_223622_create_customers_table.php
+++ database/migrations/2024_12_13_223622_create_customers_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->string('dni')->index();
             $table->string('address')->index();
             $table->string('cellphone')->index();
-            $table->string('email')->index();
             $table->timestamps();
         });
     }
